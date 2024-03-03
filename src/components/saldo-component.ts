95ea113d822c78640c4486f1d02c577480baae6b
Rodrigo Vieira Dos Santos <rodrigo.vieira33@hotmail.com>
2024-03-03 16:46:24 -0300

    Finalizado

--- src/components/saldo-component.ts
+++ src/components/saldo-component.ts
@@ -1,7 +1,6 @@
 import { formataData, formatarValor } from "../utils/formatters.js";
 import { FormatoData } from "../types/FormatoData.js";
 
-let saldo: number = 3000
 const elementoSaldo = document.querySelector(".saldo-valor .valor") as HTMLElement;
 const elementoDataAcesso = document.querySelector('.block-saldo time') as HTMLElement;
 
